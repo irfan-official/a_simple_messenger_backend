fa2187172f94de7de1e0f369039f37c7b6728c1f
kazi Irfan <irfanofficial.work@gmail.com>
2026-02-18 14:51:21 +0600

    solve err not define

--- config/connection/db.connection.js
+++ config/connection/db.connection.js
@@ -4,5 +4,5 @@ export default async function () {
   await mongoose
     .connect(process.env.DB_URL)
     .then(() => console.log(`connected to the ${process.env.DB_NAME}`))
-    .catch(() => console.log(`Database error ===> `, err));
+    .catch((err) => console.log(`Database error ===> `, err));
 }
